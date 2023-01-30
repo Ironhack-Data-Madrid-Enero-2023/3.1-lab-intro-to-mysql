53618d95f3c1afbf6f507f2368c2f79831c1e4fa
Jorge Mendoza <jmgpgit@gmail.com>
2023-01-30 17:55:55 +0100

    corrected

--- create.sql
+++ create.sql
@@ -18,13 +18,26 @@ CREATE SCHEMA IF NOT EXISTS `lab_mysql` DEFAULT CHARACTER SET utf8mb4 COLLATE ut
 USE `lab_mysql` ;
 
 -- -----------------------------------------------------
--- Table `lab_mysql`.`model`
+-- Table `lab_mysql`.`store`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `lab_mysql`.`model` (
-  `Model_ID` INT NOT NULL,
+CREATE TABLE IF NOT EXISTS `lab_mysql`.`store` (
+  `Store_ID` VARCHAR(45) NOT NULL,
+  `Location` VARCHAR(100) NULL DEFAULT NULL,
+  PRIMARY KEY (`Store_ID`),
+  UNIQUE INDEX `Store_ID_UNIQUE` (`Store_ID` ASC) VISIBLE)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
+
+-- -----------------------------------------------------
+-- Table `lab_mysql`.`Manufacturer`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `lab_mysql`.`Manufacturer` (
+  `Model_ID` VARCHAR(45) NOT NULL,
   `Brand` VARCHAR(45) NULL,
   `Model` VARCHAR(45) NULL,
-  `Year` VARCHAR(45) NULL,
+  `Year` INT NULL,
   PRIMARY KEY (`Model_ID`))
 ENGINE = InnoDB;
 
@@ -33,10 +46,43 @@ ENGINE = InnoDB;
 -- Table `lab_mysql`.`Color`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `lab_mysql`.`Color` (
-  `Color_ID` INT NOT NULL,
+  `Color_ID` VARCHAR(15) NOT NULL,
   `Color` VARCHAR(45) NULL,
   PRIMARY KEY (`Color_ID`))
 ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `lab_mysql`.`cars`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `lab_mysql`.`cars` (
+  `VIN` INT UNSIGNED NOT NULL,
+  `store_Store_ID` VARCHAR(45) NOT NULL,
+  `Manufacturer_Model_ID` VARCHAR(45) NOT NULL,
+  `Color_Color_ID` VARCHAR(15) NOT NULL,
+  PRIMARY KEY (`VIN`, `store_Store_ID`, `Manufacturer_Model_ID`, `Color_Color_ID`),
+  UNIQUE INDEX `VIN_UNIQUE` (`VIN` ASC) VISIBLE,
+  INDEX `fk_cars_store1_idx` (`store_Store_ID` ASC) VISIBLE,
+  INDEX `fk_cars_Manufacturer1_idx` (`Manufacturer_Model_ID` ASC) VISIBLE,
+  INDEX `fk_cars_Color1_idx` (`Color_Color_ID` ASC) VISIBLE,
+  CONSTRAINT `fk_cars_store1`
+    FOREIGN KEY (`store_Store_ID`)
+    REFERENCES `lab_mysql`.`store` (`Store_ID`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_cars_Manufacturer1`
+    FOREIGN KEY (`Manufacturer_Model_ID`)
+    REFERENCES `lab_mysql`.`Manufacturer` (`Model_ID`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_cars_Color1`
+    FOREIGN KEY (`Color_Color_ID`)
+    REFERENCES `lab_mysql`.`Color` (`Color_ID`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
 
 
 -- -----------------------------------------------------
@@ -62,24 +108,16 @@ COLLATE = utf8mb4_0900_ai_ci;
 -- Table `lab_mysql`.`salespersons`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `lab_mysql`.`salespersons` (
-  `Staff_ID` INT UNSIGNED NOT NULL,
+  `Staff_ID` VARCHAR(45) NOT NULL,
   `Name` VARCHAR(45) NULL DEFAULT NULL,
-  `Store_ID` INT NULL DEFAULT NULL,
-  PRIMARY KEY (`Staff_ID`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_0900_ai_ci;
-
-
--- -----------------------------------------------------
--- Table `lab_mysql`.`store`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `lab_mysql`.`store` (
-  `Store_ID` INT UNSIGNED NOT NULL,
-  `Location` VARCHAR(100) NULL DEFAULT NULL,
-  `Salespersons_Staff_ID` INT UNSIGNED NOT NULL,
-  PRIMARY KEY (`Store_ID`, `Salespersons_Staff_ID`),
-  UNIQUE INDEX `Store_ID_UNIQUE` (`Store_ID` ASC) VISIBLE)
+  `store_Store_ID` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`Staff_ID`, `store_Store_ID`),
+  INDEX `fk_salespersons_store1_idx` (`store_Store_ID` ASC) VISIBLE,
+  CONSTRAINT `fk_salespersons_store1`
+    FOREIGN KEY (`store_Store_ID`)
+    REFERENCES `lab_mysql`.`store` (`Store_ID`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8mb4
 COLLATE = utf8mb4_0900_ai_ci;
@@ -92,61 +130,26 @@ CREATE TABLE IF NOT EXISTS `lab_mysql`.`invoices` (
   `Invoice_ID` INT UNSIGNED NOT NULL,
   `Invoice_Number` VARCHAR(45) NULL DEFAULT NULL,
   `Date` VARCHAR(45) NULL DEFAULT NULL,
-  `Customers_Customer_ID` INT UNSIGNED NOT NULL,
-  `salespersons_Staff_ID` INT UNSIGNED NOT NULL,
-  `store_Store_ID` INT UNSIGNED NOT NULL,
-  `store_Salespersons_Staff_ID` INT UNSIGNED NOT NULL,
-  PRIMARY KEY (`Invoice_ID`, `Customers_Customer_ID`, `store_Store_ID`, `store_Salespersons_Staff_ID`),
-  INDEX `fk_Invoices_Customers_idx` (`Customers_Customer_ID` ASC) VISIBLE,
+  `customers_Customer_ID` INT UNSIGNED NOT NULL,
+  `salespersons_Staff_ID` VARCHAR(45) NOT NULL,
+  `cars_VIN` INT UNSIGNED NOT NULL,
+  PRIMARY KEY (`Invoice_ID`, `customers_Customer_ID`, `cars_VIN`),
+  INDEX `fk_invoices_customers1_idx` (`customers_Customer_ID` ASC) VISIBLE,
   INDEX `fk_invoices_salespersons1_idx` (`salespersons_Staff_ID` ASC) VISIBLE,
-  INDEX `fk_invoices_store1_idx` (`store_Store_ID` ASC, `store_Salespersons_Staff_ID` ASC) VISIBLE,
-  CONSTRAINT `fk_Invoices_Customers`
-    FOREIGN KEY (`Customers_Customer_ID`)
-    REFERENCES `lab_mysql`.`customers` (`Customer_ID`),
+  INDEX `fk_invoices_cars1_idx` (`cars_VIN` ASC) VISIBLE,
+  CONSTRAINT `fk_invoices_customers1`
+    FOREIGN KEY (`customers_Customer_ID`)
+    REFERENCES `lab_mysql`.`customers` (`Customer_ID`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
   CONSTRAINT `fk_invoices_salespersons1`
     FOREIGN KEY (`salespersons_Staff_ID`)
     REFERENCES `lab_mysql`.`salespersons` (`Staff_ID`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
-  CONSTRAINT `fk_invoices_store1`
-    FOREIGN KEY (`store_Store_ID` , `store_Salespersons_Staff_ID`)
-    REFERENCES `lab_mysql`.`store` (`Store_ID` , `Salespersons_Staff_ID`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_0900_ai_ci;
-
-
--- -----------------------------------------------------
--- Table `lab_mysql`.`cars`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `lab_mysql`.`cars` (
-  `VIN` INT UNSIGNED NOT NULL,
-  `model_Model_ID` INT NOT NULL,
-  `Color_Color_ID` INT NOT NULL,
-  `invoices_Invoice_ID` INT UNSIGNED NOT NULL,
-  `invoices_Customers_Customer_ID` INT UNSIGNED NOT NULL,
-  `invoices_store_Store_ID` INT UNSIGNED NOT NULL,
-  `invoices_store_Salespersons_Staff_ID` INT UNSIGNED NOT NULL,
-  PRIMARY KEY (`VIN`, `model_Model_ID`, `Color_Color_ID`, `invoices_Invoice_ID`, `invoices_Customers_Customer_ID`, `invoices_store_Store_ID`, `invoices_store_Salespersons_Staff_ID`),
-  UNIQUE INDEX `VIN_UNIQUE` (`VIN` ASC) VISIBLE,
-  INDEX `fk_cars_model1_idx` (`model_Model_ID` ASC) VISIBLE,
-  INDEX `fk_cars_Color1_idx` (`Color_Color_ID` ASC) VISIBLE,
-  INDEX `fk_cars_invoices1_idx` (`invoices_Invoice_ID` ASC, `invoices_Customers_Customer_ID` ASC, `invoices_store_Store_ID` ASC, `invoices_store_Salespersons_Staff_ID` ASC) VISIBLE,
-  CONSTRAINT `fk_cars_model1`
-    FOREIGN KEY (`model_Model_ID`)
-    REFERENCES `lab_mysql`.`model` (`Model_ID`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_cars_Color1`
-    FOREIGN KEY (`Color_Color_ID`)
-    REFERENCES `lab_mysql`.`Color` (`Color_ID`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_cars_invoices1`
-    FOREIGN KEY (`invoices_Invoice_ID` , `invoices_Customers_Customer_ID` , `invoices_store_Store_ID` , `invoices_store_Salespersons_Staff_ID`)
-    REFERENCES `lab_mysql`.`invoices` (`Invoice_ID` , `Customers_Customer_ID` , `store_Store_ID` , `store_Salespersons_Staff_ID`)
+  CONSTRAINT `fk_invoices_cars1`
+    FOREIGN KEY (`cars_VIN`)
+    REFERENCES `lab_mysql`.`cars` (`VIN`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB
--- seeding.sql
+++ seeding.sql
@@ -6,19 +6,20 @@ values ('VW1','Volkswagen', 'Tiguan', '2015'), ('VW2','Volkswagen', 'Tiguan', '2
 ('FO1','Ford', 'Fusion', '2015'),('FO2','Ford', 'Fiesta', '2015');
 
 insert into color(Color_ID,Color)
-values ('IDred','red'), ('IDblack','black'),('IDwhite','white'),('IDblue','blue');
+values ('IDyellow','yellow'),('IDred','red'), ('IDblack','black'),('IDwhite','white'),('IDblue','blue');
 
 insert into Store(Store_ID,Location)
-values (1,'Madrid'),(2,'Barcelona'),(3,'Munich'),(4,'Beijing')
+values (1,'Madrid'),(2,'Barcelona'),(3,'Munich'),(4,'Beijing');
 
 insert into Customers(Customer_ID,Name,Phone_Number,Email,Address,City,Province,Country,Postal_code)
-values (1,'Jorge',634635206, '' , '' , 'Madrid','Madrid','Spain',28033);
+values (1,'Jorge',634635206, '' , '' , 'Madrid','Madrid','Spain',28033), (2,'Yona',234234, '234234' , '123' , 'Madrid','Madrid','Spain',28043),
+(3,'Ori',12312345, '1231233123' , '3232323' , 'Barcelona','China','World',232323);
 
 insert into salespersons(Staff_ID,Name,store_store_ID)
 values (1,'Cris',1), (2,'Axel',3), (3,'Ronald',4), (4,'Santiago',2);
 
-insert into cars(VIN,store_store_ID,manufacturer_model_ID,Color_color_ID,invoices_Invoice_ID)
-values  (2,3,'VW3','IDblack', '2'), (3,2,'AU1','IDred', '3'), (5,3,'VW3','IDblack', '4'), (4,1,'VW3','IDyellow', '5');
+insert into cars(VIN,store_store_ID,manufacturer_model_ID,Color_color_ID)
+values  (2,3,'VW3','IDblack'), (3,2,'AU1','IDred'), (5,3,'VW3','IDblack'), (4,1,'VW3','IDyellow');
 
-insert into invoices(invoice_ID,invoice_number,Date,customers_customer_ID,salespersons_staff_id)
-values  (1,1,'01/01/2018',1, 2), (2,2,'01/02/2018',1, 1), (3,3,'02/01/2018',2, 3);
+insert into invoices(invoice_ID,invoice_number,Date,customers_customer_ID,salespersons_staff_id,cars_VIN)
+values  (1,1,'01/01/2018',1, 2,2), (2,2,'12/01/2018',1, 3,4), (3,3,'12/01/2019',3, 1,5);
