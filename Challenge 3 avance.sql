0c103db734a48b108fde2b74908ff013f62203a6
Pedro Vidales <pedrovidalespr@gmail.com>
2023-01-30 19:57:40 +0100

    Seguimos

---
+++ Challenge 3 avance.sql
@@ -0,0 +1,12 @@
+insert into customers (`"Cu ID"`, `"Cust ID"`, Name, `"Phone Number"`, eMail, Address, City, Province, Country, `"Codigo Postal"`) 
+values 
+(10001, 'Pablo Picasso', '+34 636 17 63 82', NULL, 'Paseo de la Chopera, 14', 'Madrid', 'Madrid', 'Spain', '28045'),
+  (20001, 'Hedy Lamarr', '+43 1 514442250', NULL, 'Weiglgasse 10', 'Viena', 'Vienna', 'Austria', '1150'),
+  (30001, 'Katherine Johnson', '+1 202 358 0000', NULL, '300 E St SW', 'Washington, DC', 'Washington', 'United States', '20546');
+
+
+
+
+
+
+
